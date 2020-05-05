431c8e6d407b1ead8bceb9fb5d5882843c206122
18016204 <57203937+18016204@users.noreply.github.com>
2020-05-05 10:11:47 +0800

    done

--- app/src/main/java/sg/edu/rp/c346/id18016204/demosimpleclick/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id18016204/demosimpleclick/MainActivity.java
@@ -3,12 +3,39 @@ package sg.edu.rp.c346.id18016204.demosimpleclick;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvDisplay;
+    Button btnDisplay;
+    EditText etInput;
+    ToggleButton tgButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvDisplay = findViewById(R.id.textViewDisplay);
+        btnDisplay= findViewById(R.id.buttonDisplay);
+        etInput = findViewById(R.id.editTextInput);
+        tgButton= findViewById(R.id.toggleButtonEnabled);
+btnDisplay.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+
+        String stringResponse=etInput.getText().toString();
+        tvDisplay.setText(stringResponse);
     }
+});
+    }
+
 }
